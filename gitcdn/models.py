8734b0f61c0a1c8a349f56894a32307c8c340135
m.mohamadshabane@gmail.com <m.mohamadshabane@gmail.com>
2022-10-06 18:34:26 +0330

    [feat](create the database for image files]

--- gitcdn/models.py
+++ gitcdn/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='static')
+
+    def __str__(self):
+        return self.name
