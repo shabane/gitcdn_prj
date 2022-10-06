1138270c07e20ee3354ba760429539b46bd93c83
m.mohamadshabane@gmail.com <m.mohamadshabane@gmail.com>
2022-10-06 19:53:25 +0330

    [feat](create new function for create uniq names)

---
+++ gitcdn/moduls.py
@@ -0,0 +1,22 @@
+"""This file contain custom useful modules"""
+import uuid
+import glob
+from gitcdn_prj.settings import STATICFILES_DIR
+from os.path import basename
+
+
+def unamer(*args, **kwargs):
+    """this function will check the statich dirs and base on files on there make uniq name
+    :return uniq string
+    """
+
+    files = []
+    for dir in STATICFILES_DIR:
+        for file in (glob.glob(f'{dir}/*')):
+            files.append(basename(file)[:basename(file).find('.')])
+
+    choos = str(uuid.uuid4())
+    while choos in files:
+        choos = str(uuid.uuid4())
+
+    return choos
