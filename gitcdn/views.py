93153e55ad2cbfa3113e9c47dc198b2ef106e116
m.mohamadshabane@gmail.com <m.mohamadshabane@gmail.com>
2022-10-07 06:32:52 +0330

    [feat](use file hash as it`s name)

--- gitcdn/views.py
+++ gitcdn/views.py
@@ -3,11 +3,11 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Image
 from .serializer import ImageSerializer
 from rest_framework.response import Response
-from gitcdn_prj.settings import SAVE_TO_DB
+from gitcdn_prj.settings import SAVE_TO_DB, REPO, OWNER, TOKEN
 from .moduls import unamer
 from .github import Github
 import os
-import base64
+import hashlib
 
 
 class ImageViewSet(ModelViewSet):
@@ -17,16 +17,19 @@ class ImageViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer_class = ImageSerializer(data=request.data)
         if serializer_class.is_valid():
+            file = serializer_class.validated_data["image"].file.read()
             extension = serializer_class.validated_data['image'].name[serializer_class.validated_data['image'].name.find('.'):]
-            file_name = f'{unamer()}{extension}'
-            token = os.getenv('TOKEN', '<or hard code your token here>')
+            file_hash = hashlib.md5(file).hexdigest()
+            file_name = f'{file_hash}{extension}'
 
             if SAVE_TO_DB:
                 Image.objects.create(name=file_name, image=serializer_class.validated_data['image'])
 
-            git = Github('bit-orbit', 'CDN', token)
-            file = serializer_class.validated_data['image'].file.read()
+            git = Github(OWNER, REPO, TOKEN)
+            file = file
             res = git.insert(f'files/{file_name}', file, f'add image {file_name}')
+
+            #TODO: check if 200 or 201
 
             res = {
                 'status': res.status_code,
