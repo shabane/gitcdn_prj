4089bcec5014c134f7f4de24d219bd37b2da3e5a
m.mohamadshabane@gmail.com <m.mohamadshabane@gmail.com>
2022-10-07 05:42:15 +0330

    [feat](create Github class and the 'insert' func):
    	this class extend 'cdnInterface' and impliment its feachers.
    	for now we just create insert func

---
+++ gitcdn/github.py
@@ -0,0 +1,34 @@
+"""the github actual class"""
+from .cdninterface import CdnInterface
+import requests
+import base64
+import json
+
+
+class Github(CdnInterface):
+    """this is the class of github"""
+
+    def __init__(self, owner, repo, token):
+        CdnInterface.__init__(self, owner, repo)
+        self.token = token
+        self._apiUrl = f'https://api.github.com/repos/{self.owner}/{self.repo}/contents'
+        self.headers= {
+            'Authorization': f'Bearer {token}',
+            'Accept': 'application/vnd.github+json',
+        }
+
+    def insert(self, path, file, message):
+        data = {
+            'content': base64.b64encode(file).decode(),
+            'message': message
+        }
+
+        data = json.dumps(data)
+
+        res = requests.put(f'{self._apiUrl}/{path}', headers=self.headers, data=data)
+        return res
+
+    @property
+    def apiUrl(self):
+        """getter and setter for apiUrl"""
+        return self._apiUrl
