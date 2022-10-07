0c320e391aa79af6fb05157377641e9efff642c0
m.mohamadshabane@gmail.com <m.mohamadshabane@gmail.com>
2022-10-07 05:41:28 +0330

    [feat](add 'static dir' path, bool for save to db)

--- gitcdn_prj/settings.py
+++ gitcdn_prj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,8 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom settings
+
+SAVE_TO_DB = False  #indicate that either save files to database or not
